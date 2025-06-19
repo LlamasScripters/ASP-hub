81fa00f963b591620f9411d6399cc6db4cfc3157
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2025-06-20 00:49:59 +0200

    feat(deploy): optimise dev setup + preparation for prod deployment

--- server/env.d.ts
+++ server/env.d.ts
@@ -5,7 +5,7 @@ declare namespace NodeJS {
 		MONGO_URI: string;
 		BETTER_AUTH_SECRET: string;
 		BETTER_AUTH_URL: string;
-		CLIENT_URL: string;
+		HOST: string;
 		BREVO_API_KEY: string;
 		S3_ACCESS_KEY_ID: string;
 		S3_SECRET_ACCESS_KEY: string;
